0ed9c55967566e58f05d7d1126cbe58137925527
snerks <martin.github@outlook.com>
2021-04-05 14:39:42 +0100

    Initila commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+
+// import { ProfileEditorComponent } from "./profile-editor/profile-editor.component";
+// import { HomeComponent } from "./home/home.component";
+// import { ShowlistComponent } from "./showlist/showlist.component";
+// import { ShowdetailComponent } from "./showdetail/showdetail.component";
+// import { ShowConfirmDeleteComponent } from "./show-confirm-delete/show-confirm-delete.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  // { path: "create", component: ProfileEditorComponent },
+  // { path: "list/:days", component: ShowlistComponent },
+  // { path: "detail/:id", component: ShowdetailComponent },
+  // // { path: "detail", component: ShowdetailComponent }
+  // { path: "showconfirmdelete/:id", component: ShowConfirmDeleteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/gig.service.ts
@@ -0,0 +1,82 @@
+// import { Injectable } from '@angular/core';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class GigService {
+
+//   constructor() { }
+// }
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from "rxjs";
+
+import { ShowsInfo } from "./models";
+// import { ShowsInfoResult } from "./mock-shows-info";
+
+@Injectable({
+  providedIn: "root"
+})
+export class GigService {
+  // private showsInfoUrl =
+  //   "https://snerks.github.io/recommended-shows-ts01/recommended-shows.json";
+
+  private showsInfoGithubUrl =
+    "https://snerks.github.io/recommended-shows-ts01/recommended-shows.json";
+
+  // private showsInfoUrl = "https://api.myjson.com/bins/6blgs";
+  // private showsInfoUrl = "https://api.myjson.com/bins/BOGUS";
+  private showsInfoUrl = "https://show01-cd72d.firebaseio.com/.json";
+
+  showsInfo: ShowsInfo | null = null;
+  errorMessage: string | undefined;
+
+  constructor(private http: HttpClient) {
+    const getSuccessFn = (showsInfo: ShowsInfo) => {
+      // console.log("getShowsInfo:getSuccessFn");
+      // console.log(showsInfo);
+
+      this.showsInfo = showsInfo;
+    };
+
+    const getErrorFn = (error: any) => {
+      // console.log("getShowsInfo:errorFn");
+      // console.log(error);
+
+      this.errorMessage = error.message;
+    };
+
+    const getCompleteFn = () => {
+      // console.log("getShowsInfo:completeFn");
+    };
+
+    this.getShowsInfo().subscribe(getSuccessFn, getErrorFn, getCompleteFn);
+    // this.putShowsInfo().subscribe(getSuccessFn, getErrorFn, getCompleteFn);
+  }
+
+  // getShowsInfo(): ShowsInfo {
+  //   return ShowsInfoResult;
+  // }
+
+  set ShowsInfo(showsInfo: ShowsInfo) {
+    this.showsInfo = showsInfo;
+  }
+
+  getShowsInfo(): Observable<ShowsInfo> {
+    // return this.http.get<ShowsInfo>(this.showsInfoUrl).pipe(
+    //   tap(heroes => this.log("fetched heroes")),
+    //   catchError(this.handleError("getHeroes", []))
+    // );
+
+    if (this.showsInfo == null) {
+      return this.http.get<ShowsInfo>(this.showsInfoUrl);
+      // return this.http.get<ShowsInfo>(this.showsInfoGithubUrl);
+    }
+
+    return of(this.showsInfo);
+  }
+
+  putShowsInfo(showsInfo: ShowsInfo): Observable<ShowsInfo> {
+    return this.http.put<ShowsInfo>(this.showsInfoUrl, showsInfo);
+  }
+}
